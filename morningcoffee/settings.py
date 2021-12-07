8464afb4c766fe6fe2ac67518007a005436756a3
arthurchieppe <arthurgc1@al.insper.edu.br>
2021-12-07 00:07:49 -0300

    Arquivos heroku

--- morningcoffee/settings.py
+++ morningcoffee/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@==+jw+*bb9dja4r0$ns3731$c7jcmyjv^sirtb$gl58vivb+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ancient-cove-96144.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -77,11 +78,19 @@ WSGI_APPLICATION = 'morningcoffee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/morningcoffee?user=arthurchieppe&password=password',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
